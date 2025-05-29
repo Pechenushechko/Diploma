a6873c3b58ebec70e383fce8b6e92dfaf3ef5796
XaXa <mrhald70@gmail.com>
2025-05-29 22:05:01 +0500

    KARZINA

--- store/models.py
+++ store/models.py
@@ -1,4 +1,4 @@
-from mongoengine import Document, StringField, DecimalField, DateTimeField
+from mongoengine import Document, StringField, DecimalField, DateTimeField, ListField, FloatField, EmbeddedDocument, fields
 from datetime import datetime
 
 CATEGORY_CHOICES = [
@@ -9,6 +9,8 @@ CATEGORY_CHOICES = [
     ('Лекарства', 'Лекарства'),
     ('Прочее', 'Прочее'),
 ]
+
+ORDER_STATUSES = ['Новый', 'В обработке', 'Доставлен', 'Отменён']
 
 class Product(Document): 
     name = StringField(required=True)
@@ -33,3 +35,22 @@ class ProductLog(Document):
         'collection': 'product_logs',
         'abstract' : False
     }
+
+class OrderItem(EmbeddedDocument):
+    product_id = fields.ObjectIdField(required=True)
+    name = fields.StringField(required=True)
+    price = fields.DecimalField(required=True, precision=2)
+    quantity = fields.IntField(required=True, min_value=1)
+
+class Order(Document):
+    user_id = fields.StringField() # ID пользователя, который сделал заказ
+    items = fields.EmbeddedDocumentListField(OrderItem, required=True)
+    full_name = fields.StringField(required=True)
+    phone = fields.StringField(required=True)
+    address = fields.StringField(required=True)
+    total_price = FloatField(default=0)
+    status = fields.StringField(choices= ORDER_STATUSES, default='Новый')
+    created_at = fields.DateTimeField(default=datetime.utcnow)
+
+    def calculate_total(self):
+        self.total_price = sum(item.price * item.quantity for item in self.items)
--- store/urls.py
+++ store/urls.py
@@ -11,7 +11,17 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
     path('product/<str:product_id>/', views.product_detail, name='product_detail'),
+    path('cart/add/<str:product_id>/', views.add_to_cart, name='add_to_cart'),
+    path('cart/add/<str:product_id>/', views.add_to_cart, name='add_to_cart'),
+    path('cart/update/<str:product_id>/', views.update_cart, name='update_cart'),
+    path('cart/remove/<str:product_id>/', views.remove_from_cart, name='remove_from_cart'),
+    path('cart/', views.cart_view, name='cart_view'),
     path('admin/', admin.site.urls),
+    path('dashboard/orders/', views.order_list, name='order_list'),
+    path('dashboard/orders/<str:order_id>/update/', views.update_order_status, name='update_order_status'),
+
+    # path('cart/reset/', views.reset_cart),
+
 
 ]
 
